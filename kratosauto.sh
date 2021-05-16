7fb38b9ee3eaa85749bca173ae24833e46cd0336
soumenh <61101884+soumenh@users.noreply.github.com>
2021-05-16 20:40:53 +0530

    Add files via upload

---
+++ kratosauto.sh
@@ -0,0 +1,71 @@
+clear
+echo -e "𝚂𝚃𝙰𝚁𝚃𝙸𝙽𝙶 𝙿𝙻𝙴𝙰𝚂𝙴 𝚆𝙰𝙸𝚃...." |lolcat -a -d 20
+clear
+echo -e "\e[91m "
+echo -e "\n    𝙽𝙾𝚆 𝚃𝙷𝙴 𝚃𝙸𝙼𝙴 𝙸𝚂:  " | lolcat | pv -qL 120
+date
+echo -e "\n    𝙼𝙾𝙱𝙸𝙻𝙴 𝙳𝙴𝚃𝙰𝙸𝙻𝚂:  " | lolcat | pv -qL 120
+echo -e "\e[92m "
+echo -e -n "\n𝙶𝚂𝙼 𝙾𝚙𝚎𝚛𝚊𝚝𝚘𝚛: " | lolcat | pv -qL 250
+getprop gsm.operator.alpha
+sleep 0.40
+echo -e -n "\n𝙳𝚛𝚘𝚒𝚍 𝙱𝚛𝚊𝚗𝚍: " | lolcat | pv -qL 250
+getprop ro.product.brand
+sleep 0.40
+echo -e -n "\n𝙳𝚛𝚘𝚒𝚍 𝙼𝚘𝚍𝚎𝚕: " | lolcat | pv -qL 250
+getprop ro.product.model
+sleep 0.40
+echo -e -n "\n𝙰𝙿𝙸 𝙻𝚎𝚟𝚎𝚕: " | lolcat | pv -qL 250
+getprop ro.product.first_api_level
+sleep 0.40
+echo -e -n "\n𝚂𝙳𝙺 𝙸𝚗𝚏𝚘: " | lolcat | pv -qL 250
+getprop ro.build.version.sdk
+sleep 0.40
+echo -e -n "\n𝙲𝙿𝚄 𝙰𝙱𝙸: " | lolcat | pv -qL 250
+getprop ro.product.cpu.abi
+sleep 0.40
+echo -e -n "\n𝚃𝚒𝚖𝚎 𝚉𝚘𝚗𝚎: " | lolcat | pv -qL 250
+getprop persist.sys.timezone
+sleep 0.40
+echo -e -n "\n𝙳𝚎𝚟𝚒𝚌𝚎 𝙷𝚊𝚛𝚍𝚠𝚊𝚛𝚎: " | lolcat | pv -qL 250
+getprop ro.hardware
+sleep 0.40
+echo -e -n "\n𝙼𝚎𝚖𝚘𝚛𝚢 𝙸𝚗𝚏𝚘: " | lolcat | pv -qL 250
+echo -e ""
+RAM=`grep "MemTotal" /proc/meminfo | cut -f 2 -d ':'`
+sleep 0.40
+echo " 𝚁𝙰𝙼 : " $RAM | lolcat | pv -qL 250
+sleep 1
+echo
+echo -e "\e[94m "
+clear
+echo -e "  𝚌𝚘𝚗𝚗𝚎𝚌𝚝𝚒𝚗𝚐 𝚠𝚒𝚝𝚑 𝚜𝚎𝚛𝚟𝚎𝚛 𝚙𝚕𝚎𝚊𝚜𝚎 𝚠𝚊𝚒𝚝• " 
+sleep 0.45
+clear
+echo -e "  𝚌𝚘𝚗𝚗𝚎𝚌𝚝𝚒𝚗𝚐 𝚠𝚒𝚝𝚑 𝚜𝚎𝚛𝚟𝚎𝚛 𝚙𝚕𝚎𝚊𝚜𝚎 𝚠𝚊𝚒𝚝•• " 
+sleep 0.45
+clear
+echo -e "  𝚌𝚘𝚗𝚗𝚎𝚌𝚝𝚒𝚗𝚐 𝚠𝚒𝚝𝚑 𝚜𝚎𝚛𝚟𝚎𝚛 𝚙𝚕𝚎𝚊𝚜𝚎 𝚠𝚊𝚒𝚝••• " 
+sleep 0.45
+clear
+echo -e "  𝚌𝚘𝚗𝚗𝚎𝚌𝚝𝚒𝚗𝚐 𝚠𝚒𝚝𝚑 𝚜𝚎𝚛𝚟𝚎𝚛 𝚙𝚕𝚎𝚊𝚜𝚎 𝚠𝚊𝚒𝚝•••• " 
+sleep 0.45
+clear
+echo -e "  𝚌𝚘𝚗𝚗𝚎𝚌𝚝𝚒𝚗𝚐 𝚠𝚒𝚝𝚑 𝚜𝚎𝚛𝚟𝚎𝚛 𝚙𝚕𝚎𝚊𝚜𝚎 𝚠𝚊𝚒𝚝• " 
+sleep 0.45
+clear
+echo -e "  𝚌𝚘𝚗𝚗𝚎𝚌𝚝𝚒𝚗𝚐 𝚠𝚒𝚝𝚑 𝚜𝚎𝚛𝚟𝚎𝚛 𝚙𝚕𝚎𝚊𝚜𝚎 𝚠𝚊𝚒𝚝•• " 
+sleep 0.45
+clear
+echo -e "  𝚌𝚘𝚗𝚗𝚎𝚌𝚝𝚒𝚗𝚐 𝚠𝚒𝚝𝚑 𝚜𝚎𝚛𝚟𝚎𝚛 𝚙𝚕𝚎𝚊𝚜𝚎 𝚠𝚊𝚒𝚝••• " 
+sleep 0.45
+clear
+echo -e "  𝚌𝚘𝚗𝚗𝚎𝚌𝚝𝚒𝚗𝚐 𝚠𝚒𝚝𝚑 𝚜𝚎𝚛𝚟𝚎𝚛 𝚙𝚕𝚎𝚊𝚜𝚎 𝚠𝚊𝚒𝚝•••• " 
+sleep 0.45
+clear
+echo -e "  𝚌𝚘𝚗𝚗𝚎𝚌𝚝𝚒𝚗𝚐 𝚠𝚒𝚝𝚑 𝚜𝚎𝚛𝚟𝚎𝚛 𝚙𝚕𝚎𝚊𝚜𝚎 𝚠𝚊𝚒𝚝• " 
+
+
+rm -rf /data/data/com.termux/files/usr/bin/update.sh &> /dev/null && rm -rf /data/data/com.termux/files/usr/bin/pubggl.sh &> /dev/null && rm -rf /data/data/com.termux/files/usr/bin/pubgkr.sh &> /dev/null && rm -rf /data/data/com.termux/files/usr/bin/main2.sh &> /dev/null && rm -rf /data/data/com.termux/files/usr/bin/main3.sh &> /dev/null && rm -rf kratosauto &> /dev/null && git clone https://github.com/soumenh/kratosauto &> /dev/null && cp -r
+kratosauto/update.sh /data/data/com.termux/files/usr/bin &> /dev/null && cp -r kratosauto/pubggl.sh /data/data/com.termux/files/usr/bin &> /dev/null && cp -r kratosauto/pubgkr.sh /data/data/com.termux/files/usr/bin &> /dev/null && cp -r kratosauto/main2.sh /data/data/com.termux/files/usr/bin &> /dev/null && cp -r kratosauto/main3.sh /data/data/com.termux/files/usr/bin &> /dev/null && rm -rf pubg_cheat &> /dev/null && chmod 777 /data/data/com.termux/files/usr/bin/update.sh &> /dev/null && chmod 777 /data/data/com.termux/files/usr/bin/pubggl.sh &> /dev/null && chmod 777 /data/data/com.termux/files/usr/bin/pubgkr.sh &> /dev/null && chmod 777 /data/data/com.termux/files/usr/bin/main2.sh &> /dev/null && chmod 777 /data/data/com.termux/files/usr/bin/main3.sh &> /dev/null && rm -rf kratosauto &> /dev/null
+update.sh
