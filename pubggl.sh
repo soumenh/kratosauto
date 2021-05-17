a77425c10156659a476ab5bc8da0e20ce37d392a
soumenh <61101884+soumenh@users.noreply.github.com>
2021-05-17 07:44:12 +0530

    Add files via upload

---
+++ pubggl.sh
@@ -0,0 +1,102 @@
+clear
+  echo ""
+if [[ -d /data/adb/magisk ]]; then
+echo -e " 𝚈𝙾𝚄 𝙰𝚁𝙴 𝚁𝙾𝙾𝚃𝙴𝙳 𝚄𝚂𝙴𝚁 " 
+echo -e " 𝚂𝚝𝚊𝚛𝚝𝚎𝚍 𝚛𝚘𝚘𝚝 𝚙𝚛𝚘𝚐𝚛𝚎𝚜𝚜..." && sleep 2 
+if [[ -d /data/data/com.tencent.ig ]]; then
+clear
+echo -e " [𝙸𝚗𝚓𝚎𝚌𝚝𝚒𝚗𝚐 𝙵𝚒𝚡 𝚌𝚛𝚊𝚜𝚑]" && sleep 1 && clear
+chmod 000 /data/data/com.tencent.ig/files/tss_tmp/* &> /dev/null
+rm -rf /data/data/com.tencent.ig/app_crashrecord &> /dev/null
+touch /data/data/com.tencent.ig/app_crashrecord &> /dev/null
+sleep 1
+echo -e " [𝚌𝚛𝚊𝚜𝚑 𝚏𝚒𝚡𝚎𝚍 𝚠𝚒𝚝𝚑𝚘𝚞𝚝 𝚊𝚗𝚢 𝚎𝚛𝚛𝚘𝚛]" && sleep 1 && clear
+echo -e " [𝙱𝙰𝙽 𝚁𝙴𝙼𝙾𝚅𝙸𝙽𝙶 𝙿𝚁𝙾𝙲𝙴𝚂𝚂 𝚂𝚃𝙰𝚁𝚃𝙴𝙳] "
+echo -e " [𝙾𝙵𝙵𝙻𝙸𝙽𝙴 𝙱𝙰𝙽 𝚁𝙴𝙼𝙾𝚅𝙴𝙳] "
+rm -rf /sdcard/.backups &> /dev/null
+rm -rf /sdcard/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/SaveGames/Match &> /dev/null
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/login-identifier.txt &> /dev/null
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/cacheFile.txt &> /dev/null
+ rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/login-identifier.txt &> /dev/null
+ rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/vmpcloudconfig.json &> /dev/null
+ rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/ProgramBinaryCache &> /dev/null
+echo -e " [𝟹𝚁𝙳 𝙿𝙰𝚁𝚃𝚈 𝙱𝙰𝙽 𝚁𝙴𝙼𝙾𝚅𝙴𝙳]"
+rm -rf /proc/sys/fs/inotify/max_user_wathes &> /dev/null
+echo "0-8192"> /proc/sys/fs/inotify/max_user_wathes &> /dev/null
+echo -e " [𝟷𝟶/𝟹𝟶 𝙼𝙸𝙽 𝙱𝙰𝙽 𝚁𝙴𝙼𝙾𝚅𝙴𝙳]"
+echo -e " 𝙰𝙻𝙻 𝙱𝙰𝙽 𝚁𝙴𝙼𝙾𝚅𝙴𝙳 "
+sleep 2 && echo "" && clear && echo -e " [𝙿𝚄𝙱𝙶 𝙺𝚁 𝚆𝙸𝙻𝙻 𝙰𝚄𝚃𝙾 𝚂𝚃𝙰𝚁𝚃 𝚆𝙸𝚃𝙷𝙸𝙽 𝟻 𝚂𝙴𝙲]"
+rm -rf /data/data/com.tencent.ig/app_crashrecord &> /dev/null
+touch /data/data/com.tencent.ig/app_crashrecord &> /dev/null
+rm -rf /data/data/com.tencent.ig/lib/libzlib.so &> /dev/null
+cp -r /data/data/com.termux/files/usr/bin/kratosbullet.py /data/data/com.tencent.ig/lib/libzlib.so &> /dev/null
+chmod -R 755 /data/data/com.tencent.ig/lib/* &> /dev/null
+sleep 5
+am start -n com.tencent.ig/com.epicgames.ue4.SplashActivity &> /dev/null
+clear
+echo -e " [𝙱𝚁𝚄𝙲𝙷 𝙲𝙻𝙴𝙰𝙽𝙸𝙽𝙶 𝚂𝚃𝙰𝚁𝚃𝙴𝙳]" && sleep 15
+echo -e "   𝙿𝚛𝚎𝚜𝚜 𝙲𝚃𝚁𝙻+𝙲 𝚝𝚘 𝚜𝚝𝚘𝚙!"      
+i=1
+j=0
+sleep 10
+PACKAGE='com.tencent.ig'
+while [ $(pidof $PACKAGE) ]
+do
+clear
+rm -rf /data/data/com.tencent.ig/app_bugly 2> /dev/null
+rm -rf /data/data/com.tencent.ig/cache 2> /dev/null
+rm -rf /data/data/com.tencent.ig/code_cache 2> /dev/null
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/cache 2> /dev/null
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/tbslog 2> /dev/null
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Logs 2> /dev/null
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/StatEventReportedFlag 2> /dev/null
+rm -rf /sdcard/.time 2> /dev/null
+rm -rf /sdcard/QTAudioEngine 2> /dev/null
+rm -rf /sdcard/Android/data/com.tencent.ig/files/cacheFile.txt 2> /dev/null
+rm -rf /sdcard/Android/data/com.tencent.ig/files/login-identifier.txt 2> /dev/null
+rm -rf /sdcard/Android/data/com.tencent.ig/cache 2> /dev/null
+rm -rf /sdcard/Android/data/com.tencent.ig/files/ProgramBinaryCache 2> /dev/null
+rm -rf /sdcard/Android/data/com.tencent.ig/files/TGPA 2> /dev/null
+rm -rf /sdcard/Android/data/com.tencent.ig/files/MMKV 2> /dev/null
+rm -rf /sdcard/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Logs 2> /dev/null
+rm -rf /sdcard/Android/data/com.tencent.ig/files/cacheFile.txt 2> /dev/null
+rm -rf /sdcard/Android/data/com.tencent.ig/files/login-identifier.txt 2> /dev/null
+rm -rf /sdcard/Android/data/com.tencent.ig/cache 2> /dev/null
+rm -rf /sdcard/Android/data/com.tencent.ig/files/tbslog 2> /dev/null
+rm -rf /sdcard/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Logs 2> /dev/null
+rm -rf /src/main/java/com/google/errorprone/annotations 2> /dev/null
+rm -rf /src/main/java/com/google/errorprone/annotations/concurrent 2> /dev/null
+rm -rf /third_party.java_src.error_prone.project.annotations.Google_internal 2> /dev/null
+rm -rf /sdcard/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Intermediate 2> /dev/null
+echo " "
+sleep 15
+echo " 𝙻𝙾𝙾𝙿𝙸𝙽𝙶 𝙱𝚈𝙿𝙰𝚂𝚂 $c .........."
+echo " "
+c=$((c+1))
+done
+if [ ! $(pidof $PACKAGE) ]; then
+rm -r /data/data/com.tencent.ig/lib/libzlib.so > /dev/null 2>&1
+cp -r /data/data/com.termux/files/usr/bin/kratosunbullet.py /data/data/com.tencent.ig/lib/libzlib.so &> /dev/null
+chmod -R 755 /data/data/com.tencent.ig/lib/* &> /dev/null
+am start -n com.termux/com.termux.app.TermuxActivity &> /dev/null 
+clear & echo " 𝚈𝙾𝚄 𝙰𝚁𝙴 𝙲𝙻𝙾𝚂𝙴𝙳 𝚈𝙾𝚄𝚁 𝙶𝙰𝙼𝙴, 𝙿𝙻𝙴𝙰𝚂𝙴 𝚆𝙰𝙸𝚃....." && sleep 5
+clear
+clear & echo " 𝙳𝙾𝙽𝙴, 𝙽𝙾𝚆 𝚈𝙾𝚄 𝙲𝙰𝙽 𝙶𝙾😊" 
+exit 0
+
+fi
+sleep 1
+final=$((c/6))
+echo $final
+
+
+else
+echo -e " 𝚈𝙾𝚄 𝙰𝚁𝙴 𝙽𝙾𝚃 𝚄𝚂𝙸𝙽𝙶 𝙿𝚄𝙱𝙶 𝙺𝚁, 𝙵𝙸𝚁𝚂𝚃 𝙸𝙽𝚂𝚃𝙰𝙻𝙻 𝙿𝚄𝙱𝙶 𝙺𝚁 𝙰𝙽𝙳 𝙲𝙾𝙼𝙴 𝙷𝙴𝚁𝙴 "
+exit 0
+fi
+
+else
+   echo -e " 𝚈𝙾𝚄 𝙰𝚁𝙴 𝙽𝙾𝙽-𝚁𝙾𝙾𝚃𝙴𝙳 𝚄𝚂𝙴𝚁 " 
+   
+exit 0
+fi
